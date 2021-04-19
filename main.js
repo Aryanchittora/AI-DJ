39b771f15e4e4301f9c432631aff03ec8151749d
Aryanchittora <72014634+Aryanchittora@users.noreply.github.com>
2021-04-19 14:49:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ l_wristY = 0;
 r_wristX = 0;
 r_wristY = 0;
 score_lWrist = 0;
+score_rWrist = 0;
 
 function preload() {
     song = loadSound("Invincible.mp3");
@@ -41,21 +42,46 @@ function poses(results) {
         console.log("Right Wrist Y = " + r_wristY);
 
         score_lWrist = results[0].pose.keypoints[9].score;
+        score_rWrist = results[0].pose.keypoints[10].score;
         console.log("SCORE left Wrist = " + score_lWrist);
+        console.log("SCORE Right Wrist = " + score_rWrist);
     }
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    
     fill('#57ff97');
     stroke('#57ff97');
-    if (score_lWrist > 0.2) {
+    
+    if (score_lWrist > 0.02) {
         circle(l_wristX, l_wristY, 15);
 
         var1 = floor(Number(l_wristY));
         volume = var1/500;
         document.getElementById("volume_val").innerHTML = "Volume = " + volume;
         song.setVolume(volume);    
+    }
+
+    if (score_rWrist > 0.02) {
+        circle(r_wristX, r_wristY, 15);
+        
+        if (r_wristY > 0 && r_wristY <= 200) {
+            document.getElementById("speed_val").innerHTML = "Speed = 2x";
+            song.rate(2);
+        }
+        else if (r_wristY > 200 && r_wrist <= 300) {
+            document.getElementById("speed_val").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+        }
+        else if (r_wristY > 300 && r_wrist <= 400) {
+            document.getElementById("speed_val").innerHTML = "Speed = 1x";
+            song.rate(1);
+        }
+        else if (r_wristY > 400 && r_wrist <= 500) {
+            document.getElementById("speed_val").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+        }
     }
 }
 
